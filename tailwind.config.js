5b3c63958394fbb9402598cd4e920e12cf0c72d8
TimothyBelekollie <belekollietimothy2@gmail.com>
2023-06-15 14:39:48 +0200

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  content: ["./src/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        "footer-bg": "#f2f2f2",
+        "footer-text-color": "#616161",
+        
+      }
+    },
+    // container: {
+    //   center: true,
+    //   padding: {
+    //     DEFAULT: '20px',
+    //     md: "50px"
+    //   }
+    // }
+  },
+  plugins: [],
+}
